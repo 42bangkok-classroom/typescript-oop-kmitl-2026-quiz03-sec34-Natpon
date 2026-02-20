7adde93fbe979ee9238943779cc6d0e4009b6d2c
Natpon <25012514amonwan@gmail.com>
2026-02-20 17:30:43 +0700

    dddd

--- src/practicals/p01/car.ts
+++ src/practicals/p01/car.ts
@@ -1,1 +1,17 @@
-export class Car {}
+export class Car {
+    brand?: string;
+    model?: string;
+    start(): string{
+        return `${this.brand} ${this.model} is starting...`
+    }
+    accelerate(): string{
+        return `${this.brand} ${this.model} is accelerating...`
+    }
+    brake(): string{
+        return `${this.brand} ${this.model} is break...`
+    }
+    stop(): string{
+        return `${this.brand} ${this.model} is stop...`
+    }
+
+}
