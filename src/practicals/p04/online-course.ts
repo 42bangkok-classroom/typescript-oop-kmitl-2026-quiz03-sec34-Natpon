496d69b544ecf03329046254b940d0ae185c9bd5
Natpon <25012514amonwan@gmail.com>
2026-02-20 18:33:46 +0700

    dddd

--- src/practicals/p04/online-course.ts
+++ src/practicals/p04/online-course.ts
@@ -1,1 +1,49 @@
-export class OnlineCourse {}
+export class OnlineCourse {
+    public courseName: string;
+    public maxStudents: number;
+    private enrolledStudents: number;
+    private isOpen: boolean;
+    constructor(nam: string , max: number){
+        this.courseName = nam;
+        this.maxStudents = max;
+    }
+    /*public OnlineCourse(nam: string,max: number){
+
+    }*/
+    public enroll(): boolean{
+        this.maxStudents--;
+        if(this.maxStudents >= 0){
+            return true;
+        }
+        else{
+            return false ;
+        }
+
+    }
+    public closeCourse(): void{
+        this.getCourseStatus = "Closed";
+
+    }
+    public getAvailableSeats(): number{
+        
+        if(this.maxStudents <= 0){
+            this.maxStudents = 0;
+        }
+        return this.maxStudents;
+
+    }
+    public getCourseStatus(): string{
+        if(this.maxStudents  ){
+            return "Closed";
+        }
+        return "Open";
+    }
+    private canEnroll(): boolean{
+        if(this.getCourseStatus !== "Closed"){
+            return true;
+        }
+        return false;
+    }
+
+    
+}
