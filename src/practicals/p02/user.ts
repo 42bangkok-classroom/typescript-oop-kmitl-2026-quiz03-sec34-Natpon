052114df4a9a61f818316ad656d71bc1d12d27a6
Natpon <25012514amonwan@gmail.com>
2026-02-20 17:51:15 +0700

    dddd

--- src/practicals/p02/user.ts
+++ src/practicals/p02/user.ts
@@ -1,1 +1,26 @@
-export class User {}
+export class User {
+    firstname?: string = "";
+    lastname?: string = "";
+    private age: number = 0;
+    static BIRTH_YEAR: number = 2000;
+    setFirstname(first: string){
+        this.firstname = first;
+
+    }
+    setLastname(last: string):string{
+       return this.lastname = last;
+
+    }
+    setAge(num: number): number{
+        return this.age = num;
+
+    }
+    getAge(): string{
+        return `${this.age}`
+
+    }
+    getFullName(): string{
+        return `${this.firstname} ${this.lastname}`
+
+    }
+}
