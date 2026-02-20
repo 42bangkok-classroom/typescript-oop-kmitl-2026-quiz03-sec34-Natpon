579e09507f5f7beb6380d5db1fa3c4f0544c50ed
Natpon <25012514amonwan@gmail.com>
2026-02-20 18:48:32 +0700

    dddd

--- src/practicals/p03/character.ts
+++ src/practicals/p03/character.ts
@@ -1,1 +1,18 @@
-export class Character {}
+export class Character {
+    name: string;
+    health: number = 100;
+    constructor (nam: string){
+        this.name = nam;
+    }
+    getName(){
+        return this.name;
+
+    }
+    getHealth(){
+        return this.health;
+
+    }
+    receiveDamage(da: number){
+        this.health--;
+    }
+}
--- src/practicals/p03/warrior.ts
+++ src/practicals/p03/warrior.ts
@@ -2,4 +2,19 @@ import { Character } from "./character";
 
 export class Warrior extends Character {
   // TODO: implement class properties, constructor with super(...), and methods
+  weapon: string;
+  constructor(name: string,weapon: string,health: number){
+    super(name)
+    this.weapon = weapon;
+    this.health = health;
+
+  }
+  getWeapon(weapon: string){
+    this.weapon = weapon;
+  }
+  receiveDamage(da: number): void {
+    this.health -= da;
+
+    
+  }
 }
